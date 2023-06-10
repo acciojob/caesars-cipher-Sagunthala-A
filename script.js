bb0ee18c6cb67dced9f16ee4a0db733542a7a158
Sagunthala-A <117196512+Sagunthala-A@users.noreply.github.com>
2023-06-10 17:18:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-// Your Script here.
+// // Your Script here.
+
+// const lookup = {
+//   A: "N",
+//   B: "O",
+//   C: "P",
+//   D: "Q",
+//   E: "R",
+//   F: "S",
+//   G: "T",
+//   H: "U",
+//   I: "V",
+//   J: "W",
+//   K: "X",
+//   L: "Y",
+//   M: "Z",
+//   N: "A",
+//   O: "B",
+//   P: "C",
+//   Q: "D",
+//   R: "E",
+//   S: "F",
+//   T: "G",
+//   U: "H",
+//   V: "I",
+//   W: "J",
+//   X: "K",
+//   Y: "L",
+//   Z: "M",
+//   "?": "?",
+//   ",": ",",
+// };
+
+// function rot13(encodedStr) {
+//   let decodedArr = []; // Your Result goes here
+//   // Only change code below this line
+
+//   return; //return decodedArr
+// }
+
+// // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// // console.log(rot13("SERR YBIR? NPPVBWBO"));
+
+// // Do not change this line
+// window.rot13 = rot13;
+
+
+
+
+
+
 
 const lookup = {
   A: "N",
@@ -36,20 +87,18 @@ function rot13(encodedStr) {
 
   for (let i = 0; i < encodedStr.length; i++) {
     let char = encodedStr[i].toUpperCase();
-
-    if (lookup.hasOwnProperty(char)) {
-      decodedArr.push(lookup[char]);
-    } else {
-      decodedArr.push(char);
-    }
+    let decodedChar = lookup[char] || char;
+    decodedArr.push(decodedChar);
   }
 
   return decodedArr.join("");
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
 console.log(rot13("SERR YBIR? NPPVBWBO"));
 
-// Do not change this line
-window.rot13 = rot13;
+
+
+
+
+
+
